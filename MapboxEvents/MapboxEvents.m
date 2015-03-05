056e50eacad4f919f58726cc1e988da19ccdbeb9
Brad Leege <bleege@gmail.com>
2015-03-05 10:12:56 -0600

    Initial data model setup

--- MapboxEvents/MapboxEvents.m
+++ MapboxEvents/MapboxEvents.m
@@ -8,6 +8,25 @@
 
 #import "MapboxEvents.h"
 
+@interface MapboxEvents()
+
+@property (atomic) NSMutableArray *queue;
+@property (atomic) NSInteger *flushAt;
+@property (atomic) NSInteger *flushAfter;
+@property (atomic) NSString *api;
+@property (atomic) NSString *token;
+@property (atomic) NSString *instance;
+@property (atomic) NSString *anonid;
+
+@end
+
 @implementation MapboxEvents
+
+- (id) init {
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
 
 @end
